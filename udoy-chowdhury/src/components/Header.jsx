27e2277f382109b937353e227f8fccb545948897
aontor22 <udoychowdhury90413@gmail.com>
2025-09-14 23:27:46 +0600

    p

--- udoy-chowdhury/src/components/Header.jsx
+++ udoy-chowdhury/src/components/Header.jsx
@@ -4,10 +4,25 @@
  */
 
 
+/**
+ * Node modules
+ */
+
+import { useState } from 'react'
+
+/**
+ * Components
+ */
+
+import Navbar from './Navbar'
+
 import React from 'react'
 
 const Header = () => {
-  return (
+
+    const [navOpen, setNavOpen] = useState(false);
+
+    return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
         <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr, 3fr, 1fr]">
             <h1>
@@ -17,14 +32,16 @@ const Header = () => {
             </h1>
 
             <div className="relative md:justify-self-center">
-                <button className="menu-btn md:hidden" onClick={null}>
+                <button className="menu-btn md:hidden" onClick={() => setNavOpen((prev) => !prev)}>
                     <span className="material-symbols-rounded">
-                        menu
+                        {navOpen ? 'close' : 'menu'}
                     </span>
                 </button>
+
+                <Navbar navOpen={navOpen}/>
             </div>
 
-            <a href="#contact" className="">
+            <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">
                 Contact Me
             </a>
         </div>
