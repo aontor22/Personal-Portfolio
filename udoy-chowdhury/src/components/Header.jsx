646278811ad5699de061b92825e74a73e8056673
aontor22 <udoychowdhury90413@gmail.com>
2025-09-15 23:43:42 +0600

    d

--- udoy-chowdhury/src/components/Header.jsx
+++ udoy-chowdhury/src/components/Header.jsx
@@ -3,50 +3,59 @@
  * @license Apache-2.0
  */
 
-
 /**
  * Node modules
  */
 
-import { useState } from 'react'
+import { useState } from "react";
 
 /**
  * Components
  */
 
-import Navbar from './Navbar'
+import Navbar from "./Navbar";
 
-import React from 'react'
+import React from "react";
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
 
-    const [navOpen, setNavOpen] = useState(false);
-
-    return (
+  return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-        <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr, 3fr, 1fr]">
-            <h1>
-                <a href="/" className="logo">
-                    <img src="/images/logo.svg" width={40} height={40} alt="Udoy Chowdhury" />
-                </a>
-            </h1>
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr_auto_1fr]">
+        <h1>
+          <a href="/" className="logo">
+            <img
+              src="/images/logo.svg"
+              width={40}
+              height={40}
+              alt="Udoy Chowdhury"
+            />
+          </a>
+        </h1>
 
-            <div className="relative md:justify-self-center">
-                <button className="menu-btn md:hidden" onClick={() => setNavOpen((prev) => !prev)}>
-                    <span className="material-symbols-rounded">
-                        {navOpen ? 'close' : 'menu'}
-                    </span>
-                </button>
+        <div className="relative md:justify-self-center">
+          <button
+            className="menu-btn inline-grid md:hidden"
+            onClick={() => setNavOpen((prev) => !prev)}
+          >
+            <span className="material-symbols-rounded">
+              {navOpen ? "close" : "menu"}
+            </span>
+          </button>
 
-                <Navbar navOpen={navOpen}/>
-            </div>
-
-            <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">
-                Contact Me
-            </a>
+          <Navbar navOpen={navOpen} />
         </div>
-    </header>
-  )
-}
 
-export default Header
+        <a
+          href="#contact"
+          className="btn btn-secondary hidden md:inline-flex md:justify-self-end whitespace-nowrap"
+        >
+          Contact Me
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
