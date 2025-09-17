c137cc332bbc4ac2b4f2fafe34fabb9b5a677f31
aontor22 <udoychowdhury90413@gmail.com>
2025-09-17 22:26:39 +0600

    k

--- udoy-chowdhury/src/components/About.jsx
+++ udoy-chowdhury/src/components/About.jsx
@@ -1,9 +1,15 @@
+/**
+ * @copyright 2025 Udoy Chowdhury
+ * @license Apache-2.0
+ */
+
+
 import React from 'react'
 
 const About = () => {
   return (
     <div>
-      
+      About
     </div>
   )
 }
